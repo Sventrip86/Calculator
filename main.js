cad1579ae0e22111aebe1a25a99459e5463a35c2
Fabs <fab.sebis@google.com>
2023-01-20 19:34:03 +0100

    LOGIC looping numbers and adding a click event to display numbers

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const number = document.getElementsByClassName('btn_num');
-const sign = document.getElementsByClassName('btn_sings')
+const numbers = document.getElementsByClassName("btn_num");
+const sign = document.getElementsByClassName("btn_sings");
+const display = document.getElementsByClassName("calculator-display");
+let displayNumbers = [];
 
 
-addEventListener.
 
+
+// looping numbers and adding a click event, storing the buttons pressed in displayNumbers
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", function () {
+    displayNumbers.push(this.innerHTML);
+    document.getElementById("calculator-display").innerHTML = displayNumbers.join(" ");
+  });
+}
