411b2caf46bc17848d9094d970c61d8bffaf60af
Fabs <fab.sebis@google.com>
2023-01-20 19:52:54 +0100

    looping signs, sotoring them in displayNumbers[] and added a check for CE

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const numbers = document.getElementsByClassName("btn_num");
-const sign = document.getElementsByClassName("btn_sings");
+const signs = document.getElementsByClassName("btn_signs");
 const display = document.getElementsByClassName("calculator-display");
 let displayNumbers = [];
 
 
 
 
-// looping numbers and adding a click event, storing the buttons pressed in displayNumbers
+// looping numbers and adding a click event, storing the buttons pressed in displayNumbers[]
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function () {
     displayNumbers.push(this.innerHTML);
     document.getElementById("calculator-display").innerHTML = displayNumbers.join(" ");
   });
 }
+
+// looping signs and adding a click event, storing the buttons pressed in displayNumbers[]
+for (let i = 0; i < signs.length; i++) {
+    signs[i].addEventListener("click", function () {
+        
+        if(this.innerHTML === "CE"){ //check for CE press -> empty displayNumbers and put back 0 in the display
+            displayNumbers.length = 0;
+            document.getElementById("calculator-display").innerHTML = "0"
+        } else if (this.innerHTML === "C"){ //check for C press -> DO TO LOGIC !!!!!!!!!!!!!!!!!!!!!!!
+            displayNumbers.length = 0;
+            document.getElementById("calculator-display").innerHTML = "0"
+        } else {
+            displayNumbers.push(this.innerHTML);
+            document.getElementById("calculator-display").innerHTML = displayNumbers.join(" ");
+        }       
+    });
+  }
